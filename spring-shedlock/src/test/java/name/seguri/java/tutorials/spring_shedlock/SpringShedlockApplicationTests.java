0a06d88806d3d7b657ab4200d546e8436cf54ad1
Marco Seguri <seguri@users.noreply.github.com>
2021-04-15 15:29:11 +0200

    [spring-shedlock] Reformat with Google Java Code Style

--- spring-shedlock/src/test/java/name/seguri/java/tutorials/spring_shedlock/SpringShedlockApplicationTests.java
+++ spring-shedlock/src/test/java/name/seguri/java/tutorials/spring_shedlock/SpringShedlockApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringShedlockApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
