4a746b95c1601f68ac41d18b4fefc477ecf778eb
Marco Seguri <seguri@users.noreply.github.com>
2021-04-15 15:14:29 +0200

    [spring-shedlock] Import generated sources

---
+++ spring-shedlock/src/main/java/name/seguri/java/tutorials/spring_shedlock/SpringShedlockApplication.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.spring_shedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShedlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShedlockApplication.class, args);
+	}
+
+}
---
+++ spring-shedlock/src/test/java/name/seguri/java/tutorials/spring_shedlock/SpringShedlockApplicationTests.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.spring_shedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShedlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
