e400141e41c5c651e34e28fb2c61b9d73d89901f
Marco Seguri <seguri@users.noreply.github.com>
2021-08-18 12:16:53 +0200

    [spring-mongo-embedded-id] Import generated sources

---
+++ spring-mongo-embedded-id/src/main/java/name/seguri/java/tutorials/springmongoembeddedid/SpringMongoEmbeddedIdApplication.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.springmongoembeddedid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoEmbeddedIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoEmbeddedIdApplication.class, args);
+	}
+
+}
---
+++ spring-mongo-embedded-id/src/test/java/name/seguri/java/tutorials/springmongoembeddedid/SpringMongoEmbeddedIdApplicationTests.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.springmongoembeddedid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoEmbeddedIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
