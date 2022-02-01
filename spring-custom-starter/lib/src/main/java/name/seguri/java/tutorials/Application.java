c3f824025c3c38de9766212aba8455a84c587ac0
Marco Seguri <seguri@users.noreply.github.com>
2022-02-02 00:38:59 +0100

    [spring-custom-starter] Add multi-module project

---
+++ spring-custom-starter/lib/src/main/java/name/seguri/java/tutorials/Application.java
@@ -0,0 +1,7 @@
+package name.seguri.java.tutorials;
+
+public class Application {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ spring-custom-starter/web/src/test/java/name/seguri/java/tutorials/spring_custom_starter_web/ApplicationTests.java
@@ -0,0 +1,11 @@
+package name.seguri.java.tutorials.spring_custom_starter_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
