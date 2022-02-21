b735aaa6f8b0446611ad2f5ae0cabab79b4485b4
Marco Seguri <seguri@users.noreply.github.com>
2022-02-21 09:22:00 +0100

    [spring-mvc-interceptor] Add simple test with RestAssured

--- spring-mvc-interceptor/src/test/java/name/seguri/java/tutorials/springmvcinterceptor/MyInterceptorTests.java
+++ spring-mvc-interceptor/src/test/java/name/seguri/java/tutorials/springmvcinterceptor/MyInterceptorTests.java
@@ -1,22 +1,39 @@
 package name.seguri.java.tutorials.springmvcinterceptor;
 
+import static io.restassured.RestAssured.get;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class MyInterceptorTests {
 
+  @LocalServerPort int port;
+
   @Autowired private TestRestTemplate testRestTemplate;
+
+  @BeforeEach
+  public void setup() {
+    RestAssured.port = port;
+  }
 
   @Test
   public void ok() {
     var response = testRestTemplate.getForEntity("/ok", String.class);
 
     assertThat(response.getBody()).isEqualTo("OK");
+  }
+
+  @Test
+  public void ok_restassured() {
+    get("/ok").then().body(equalTo("OK"));
   }
 }
