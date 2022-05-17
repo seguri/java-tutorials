a7ab46842e78d5405500a997d57929309b9dcbdb
Marco Seguri <seguri@users.noreply.github.com>
2022-05-17 23:52:25 +0200

    [spring-web-file-upload] Import generated sources

---
+++ spring-web-file-upload/src/main/java/name/seguri/java/tutorials/springwebfileupload/SpringWebFileUploadApplication.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.springwebfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFileUploadApplication.class, args);
+	}
+
+}
---
+++ spring-web-file-upload/src/test/java/name/seguri/java/tutorials/springwebfileupload/SpringWebFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.springwebfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
