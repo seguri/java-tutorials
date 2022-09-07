6145f6b3937fcb7eb660c31be6c0960e740b589c
Marco Seguri <seguri@users.noreply.github.com>
2022-09-07 16:39:34 +0200

    [spring-openapi-generator] Initial commit

---
+++ spring-openapi-generator/src/main/java/name/seguri/java/tutorials/boundary/rest/PetsController.java
@@ -0,0 +1,42 @@
+package name.seguri.java.tutorials.boundary.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import name.seguri.java.tutorials.pets.PetDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+// You can override the mapping in application.properties
+@RestController
+@RequestMapping("${openapi.swaggerPetstore.base-path:/v1}")
+public class PetsController implements PetsApi {
+
+  private final Random random = new Random();
+  private final List<PetDto> pets = new ArrayList<>();
+
+  @Override
+  public ResponseEntity<List<PetDto>> listPets(Integer limit) {
+    return ResponseEntity.ok(pets);
+  }
+
+  @Override
+  public ResponseEntity<PetDto> showPetById(String petId) {
+    var id = Long.valueOf(petId);
+    var pet = pets.stream().filter(p -> Objects.equals(p.getId(), id)).findFirst().orElseThrow();
+    return ResponseEntity.ok(pet);
+  }
+
+  @Override
+  public ResponseEntity<Void> createPets() {
+    var id = random.nextLong();
+    var pet = new PetDto().id(id).name("pet-" + id).tag("tag-" + id);
+    pets.add(pet);
+    var location =
+        ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+    return ResponseEntity.created(location).build();
+  }
+}
