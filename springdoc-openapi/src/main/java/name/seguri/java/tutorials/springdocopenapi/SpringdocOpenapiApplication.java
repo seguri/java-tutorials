3fca2bfcfe8187b57a0dadfa7856ca8c6f3fdfaa
Marco Seguri <seguri@users.noreply.github.com>
2023-02-02 14:36:32 +0100

    [springdoc-openapi] Import sources

---
+++ springdoc-openapi/src/main/java/name/seguri/java/tutorials/springdocopenapi/SpringdocOpenapiApplication.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.springdocopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocOpenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdocOpenapiApplication.class, args);
+	}
+
+}
---
+++ springdoc-openapi/src/test/java/name/seguri/java/tutorials/springdocopenapi/SpringdocOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.springdocopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocOpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
