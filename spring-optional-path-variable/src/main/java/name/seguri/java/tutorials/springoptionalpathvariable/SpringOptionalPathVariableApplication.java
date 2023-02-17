d894a2cdac72672a84618fd609dd105fd15a70ad
Marco Seguri <seguri@users.noreply.github.com>
2023-02-17 11:20:31 +0100

    [spring-optional-path-variable] Import sources

---
+++ spring-optional-path-variable/src/main/java/name/seguri/java/tutorials/springoptionalpathvariable/SpringOptionalPathVariableApplication.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.springoptionalpathvariable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOptionalPathVariableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOptionalPathVariableApplication.class, args);
+	}
+
+}
