a11c674530e6c501d5af2400e610e819618ef45f
Marco Seguri <seguri@users.noreply.github.com>
2023-02-28 19:37:30 +0100

    [spring-conditionals] Import sources

---
+++ spring-conditionals/src/main/java/name/seguri/java/tutorials/springconditionals/SpringConditionalsApplication.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.springconditionals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConditionalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConditionalsApplication.class, args);
+	}
+
+}
---
+++ spring-conditionals/src/test/java/name/seguri/java/tutorials/springconditionals/SpringConditionalsApplicationTests.java
@@ -0,0 +1,13 @@
+package name.seguri.java.tutorials.springconditionals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConditionalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
