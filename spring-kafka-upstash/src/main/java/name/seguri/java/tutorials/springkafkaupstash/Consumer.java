e569a27cac07ad95aa7a5fe88c4b8089fd1385b5
Marco Seguri <seguri@users.noreply.github.com>
2023-04-02 21:57:02 +0200

    [spring-kafka-upstash] Consume topic from the beginning

--- spring-kafka-upstash/src/main/java/name/seguri/java/tutorials/springkafkaupstash/Consumer.java
+++ spring-kafka-upstash/src/main/java/name/seguri/java/tutorials/springkafkaupstash/Consumer.java
@@ -2,16 +2,26 @@ package name.seguri.java.tutorials.springkafkaupstash;
 
 import static org.springframework.kafka.support.KafkaHeaders.OFFSET;
 
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class Consumer {
+public class Consumer implements ConsumerSeekAware {
+
   @KafkaListener(topics = "#{systemEnvironment['KAFKA_TOPIC']}")
   public void consume(String message, @Header(OFFSET) Long offset) {
     log.info("[{}] {}", offset, message);
+  }
+
+  @Override
+  public void onPartitionsAssigned(
+      Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+    callback.seekToBeginning(assignments.keySet());
   }
 }
