97253e9b818712ef6444c023225f284577d833a4
Marco Seguri <seguri@users.noreply.github.com>
2023-04-02 15:49:31 +0200

    [spring-kafka-upstash] Initial commit

---
+++ spring-kafka-upstash/src/main/java/name/seguri/java/tutorials/springkafkaupstash/Application.java
@@ -0,0 +1,12 @@
+package name.seguri.java.tutorials.springkafkaupstash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
---
+++ spring-kafka-upstash/src/test/java/name/seguri/java/tutorials/springkafkaupstash/ApplicationTests.java
@@ -0,0 +1,11 @@
+package name.seguri.java.tutorials.springkafkaupstash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
