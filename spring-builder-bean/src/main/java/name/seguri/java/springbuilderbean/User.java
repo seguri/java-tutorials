00997468a0d13f88ff7c0a0a700afc714262d05e
Marco Seguri <seguri@users.noreply.github.com>
2023-08-03 15:38:40 +0200

    [spring-builder-bean] Initial commit

---
+++ spring-builder-bean/src/main/java/name/seguri/java/springbuilderbean/User.java
@@ -0,0 +1,66 @@
+package name.seguri.java.springbuilderbean;
+
+import java.util.Objects;
+
+public class User {
+  private String name;
+  private String email;
+
+  private User(final Builder builder) {
+    setName(builder.name);
+    setEmail(builder.email);
+  }
+
+  public static Builder newBuilder() {
+    return new Builder();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(final String email) {
+    this.email = email;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (!(o instanceof final User that)) return false;
+    return Objects.equals(name, that.name) && Objects.equals(email, that.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, email);
+  }
+
+  public static final class Builder {
+    private String name;
+    private String email;
+
+    private Builder() {}
+
+    public Builder withName(final String val) {
+      name = val;
+      return this;
+    }
+
+    public Builder withEmail(final String val) {
+      email = val;
+      return this;
+    }
+
+    public User build() {
+      return new User(this);
+    }
+  }
+}
