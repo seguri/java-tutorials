d320a814d048e6b203419ee6661bd46931549890
Marco Seguri <seguri@users.noreply.github.com>
2023-08-08 00:52:38 +0200

    [spring-sync-db-kafka-tx] Kafka works

--- spring-sync-db-kafka-tx/src/main/java/name/seguri/java/springsyncdbkafkatx/UserController.java
+++ spring-sync-db-kafka-tx/src/main/java/name/seguri/java/springsyncdbkafkatx/UserController.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
   private final UserRepository userRepository;
+  private final UserProducer userProducer;
 
-  public UserController(final UserRepository userRepository) {
+  public UserController(final UserRepository userRepository, final UserProducer userProducer) {
     this.userRepository = userRepository;
+    this.userProducer = userProducer;
   }
 
   @GetMapping("/users")
@@ -23,16 +26,31 @@ public class UserController {
   }
 
   @PostMapping("/users")
-  public User createUser(@RequestBody final User dto) {
+  public User createUser(
+      @RequestBody final User dto,
+      @RequestParam(name = "dbfail", required = false) Boolean dbfail) {
     final var next = User.builder().withName(dto.getName()).withEmail(dto.getEmail()).build();
-    return userRepository.save(next);
+    final var saved = userRepository.save(next);
+    if (Boolean.TRUE.equals(dbfail)) {
+      userRepository.generateException("Manually triggered exception");
+    }
+    userProducer.sendUser(saved);
+    return saved;
   }
 
   @PutMapping("/users/{id}")
-  public User updateUser(@PathVariable final UUID id, @RequestBody final User dto) {
+  public User updateUser(
+      @PathVariable final UUID id,
+      @RequestBody final User dto,
+      @RequestParam(name = "dbfail", required = false) Boolean dbfail) {
     final var curr = userRepository.findById(id).orElseThrow();
     final var next = User.builder(curr).withName(dto.getName()).withEmail(dto.getEmail()).build();
-    return userRepository.save(next);
+    final var saved = userRepository.save(next);
+    if (Boolean.TRUE.equals(dbfail)) {
+      userRepository.generateException("Manually triggered exception");
+    }
+    userProducer.sendUser(saved);
+    return saved;
   }
 
   @PostMapping("/dbfail")
---
+++ spring-sync-db-kafka-tx/src/main/java/name/seguri/java/springsyncdbkafkatx/UserProducer.java
@@ -0,0 +1,19 @@
+package name.seguri.java.springsyncdbkafkatx;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserProducer {
+  private static final String TOPIC = "users";
+
+  private final KafkaTemplate<String, User> kafkaTemplate;
+
+  public UserProducer(final KafkaTemplate<String, User> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public void sendUser(final User user) {
+    kafkaTemplate.send(TOPIC, user.getId().toString(), user);
+  }
+}
